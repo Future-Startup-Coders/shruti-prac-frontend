6ae38ad1d9935f63b298eecb33086fdf6b626b77
shrutighotane <138558181+shrutighotane@users.noreply.github.com>
2024-03-08 21:49:37 +0530

    Daily

---
+++ Daily challenges/Day-16/Reverse.py
@@ -0,0 +1,18 @@
+def reverse(self, x):
+        def cal(x:int):
+            x=str(x)
+            if x[0] == '-':
+                p=x[1:(len(x)+1)]
+                return 0-int(p[::-1])
+            else:
+                return int(x[::-1])
+        def cond(x):
+            return (x>-2**31) and x < (2**31)-1
+        if cond(x):
+            c = cal(x)
+            if cond(c):
+                return c
+            else:
+                return 0
+        else:
+            return 0  
