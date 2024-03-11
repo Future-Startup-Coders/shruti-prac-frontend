4415d5bb50eb41c399a2d6993bd0fa35fe4a05dd
shrutighotane <138558181+shrutighotane@users.noreply.github.com>
2024-03-11 22:40:32 +0530

    Daily

---
+++ Daily challenges/Day-19/minimum.py
@@ -0,0 +1,7 @@
+def numberGame(self, v):
+        n = len(v)
+        v.sort()
+        for i in range(0, n, 2):
+            if i + 1 < n:
+                v[i], v[i + 1] = v[i + 1], v[i]
+        return v
