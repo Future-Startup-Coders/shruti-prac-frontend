16590559c04d70e1801540b251b9df5331e36c1e
shrutighotane <138558181+shrutighotane@users.noreply.github.com>
2024-03-15 21:33:06 +0530

    Daily

---
+++ Daily challenges/Day-23/reverse.py
@@ -0,0 +1,11 @@
+def reverseOnlyLetters(self, s) :
+        u=s[::-1]
+        l=[]
+        for i in range(len(u)):
+            if u[i].isalpha():
+                l.append(u[i])
+        for i in range(len(s)):
+            if not s[i].isalpha():
+                l.insert(i,s[i])
+        p = ''.join(l)
+        return p
