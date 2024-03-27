eb6bffe9c052b24a7d53bc028f8474378135132b
shrutighotane <138558181+shrutighotane@users.noreply.github.com>
2024-03-27 23:40:20 +0530

    Daily

---
+++ Daily challenges/Day-35/35.py
@@ -0,0 +1,15 @@
+def isPalindrome(self, s) :
+        string=''
+
+        for i in s:
+            if i.isalnum():
+                string+=i.lower()
+            else:
+                continue
+        index_comp=(len(string)+1)//2
+        for i in range(index_comp):
+            if string[i]==string[len(string)-1-i]:
+                continue
+            else:
+                return False
+        return True
