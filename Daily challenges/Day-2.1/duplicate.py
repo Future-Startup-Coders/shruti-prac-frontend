4f1a35a1acd5db2021089ea2be7da7ec3356dad5
shrutighotane <138558181+shrutighotane@users.noreply.github.com>
2024-04-01 22:33:36 +0530

    Daily

---
+++ Daily challenges/Day-2.1/duplicate.py
@@ -0,0 +1,9 @@
+def removeDuplicates(self, nums) :
+        comp = 0
+        pos = 1
+        for i in range(1,len(nums)):
+            if  nums[comp] != nums[i]:       
+                nums[pos], nums[i] = nums[i], nums[pos]
+                comp = pos
+                pos += 1
+        return len(set(nums))
