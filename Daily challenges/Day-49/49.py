d259390e544fb101b0c85f956548cd5acbda3f88
shrutighotane <138558181+shrutighotane@users.noreply.github.com>
2024-04-13 22:25:56 +0530

    Daily

---
+++ Daily challenges/Day-49/49.py
@@ -0,0 +1,7 @@
+def strStr(self, haystack, needle) :
+        if len(haystack) < len(needle):
+            return -1
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
